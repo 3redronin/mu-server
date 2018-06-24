454a616999321daeb472eebc98938aef319fec16
Daniel Flower <git@danielflower.com>
2018-06-24 17:39:38 +0800

    Fixed javadoc

--- src/main/java/io/muserver/WriteCallback.java
+++ src/main/java/io/muserver/WriteCallback.java
@@ -8,11 +8,13 @@ public interface WriteCallback {
     /**
      * Called if the write failed, for example if the user closed their browser while writing to it
      * @param reason The reason for the failure
+     * @throws Exception thrown when the callback throws an exception, in which case the response will be closed.
      */
     void onFailure(Throwable reason) throws Exception;
 
     /**
      * Called if the write succeeds
+     * @throws Exception thrown when the callback throws an exception, in which case the response will be closed.
      */
     void onSuccess() throws Exception;
 
