dc2d2c4901644f347489e215ab267e1b48a253ac
Daniel Flower <git@danielflower.com>
2018-07-28 16:46:34 +0800

    Initial implementation of Expect / Accept header

--- src/main/java/io/muserver/MuServerHandler.java
+++ src/main/java/io/muserver/MuServerHandler.java
@@ -6,7 +6,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.TooLongFrameException;
 import io.netty.handler.codec.http.*;
-import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,19 +28,20 @@ class MuServerHandler extends SimpleChannelInboundHandler<Object> {
     private final AtomicReference<MuServer> serverRef;
 
     public MuServerHandler(List<AsyncMuHandler> handlers, MuStatsImpl stats, AtomicReference<MuServer> serverRef) {
-	    this.handlers = handlers;
+        this.handlers = handlers;
         this.stats = stats;
         this.serverRef = serverRef;
     }
 
-	private static final class State {
-		public final AsyncContext asyncContext;
-		public final AsyncMuHandler handler;
-		private State(AsyncContext asyncContext, AsyncMuHandler handler) {
-			this.asyncContext = asyncContext;
-			this.handler = handler;
-		}
-	}
+    private static final class State {
+        public final AsyncContext asyncContext;
+        public final AsyncMuHandler handler;
+
+        private State(AsyncContext asyncContext, AsyncMuHandler handler) {
+            this.asyncContext = asyncContext;
+            this.handler = handler;
+        }
+    }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
@@ -53,52 +53,66 @@ class MuServerHandler extends SimpleChannelInboundHandler<Object> {
     }
 
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if (msg instanceof HttpRequest) {
-			HttpRequest request = (HttpRequest) msg;
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
 
-			if (request.decoderResult().isFailure()) {
-			    stats.onInvalidRequest();
-				handleHttpRequestDecodeFailure(ctx, request.decoderResult().cause());
-			} else {
-				boolean handled = false;
+            if (request.decoderResult().isFailure()) {
+                stats.onInvalidRequest();
+                handleHttpRequestDecodeFailure(ctx, request.decoderResult().cause());
+            } else {
+
+                if (HttpUtil.is100ContinueExpected(request)) {
+                    int contentBody = request.headers().getInt("Content-Length", -1);
+                    if (contentBody < Integer.MAX_VALUE) {
+                        // TODO reject if body size too large
+                        ctx.writeAndFlush(new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.CONTINUE));
+                    } else {
+                        // TODO set values correctly
+                        ctx.writeAndFlush(new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.EXPECTATION_FAILED))
+                            .addListener(ChannelFutureListener.CLOSE);
+                        return;
+                    }
+                }
+
+                boolean handled = false;
                 NettyRequestAdapter muRequest = new NettyRequestAdapter(ctx.channel(), request, serverRef);
                 stats.onRequestStarted(muRequest);
 
                 AsyncContext asyncContext = new AsyncContext(muRequest, new NettyResponseAdaptor(ctx, muRequest), stats);
 
-				for (AsyncMuHandler handler : handlers) {
-					handled = handler.onHeaders(asyncContext, asyncContext.request.headers());
-					if (handled) {
-					    ctx.channel().attr(STATE_ATTRIBUTE).set(new State(asyncContext, handler));
-						break;
-					}
-				}
-				if (!handled) {
+                for (AsyncMuHandler handler : handlers) {
+                    handled = handler.onHeaders(asyncContext, asyncContext.request.headers());
+                    if (handled) {
+                        ctx.channel().attr(STATE_ATTRIBUTE).set(new State(asyncContext, handler));
+                        break;
+                    }
+                }
+                if (!handled) {
                     send404(asyncContext);
                     asyncContext.complete();
                 }
             }
 
-		} else if (msg instanceof HttpContent) {
-			HttpContent content = (HttpContent) msg;
-			State state = ctx.channel().attr(STATE_ATTRIBUTE).get();
-			if (state == null) {
-				log.info("Got a chunk of message for an unknown request. This can happen when a request is rejected based on headers, and then the rejected body arrives.");
-			} else {
-				ByteBuf byteBuf = content.content();
-				if (byteBuf.capacity() > 0) {
+        } else if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            State state = ctx.channel().attr(STATE_ATTRIBUTE).get();
+            if (state == null) {
+                log.info("Got a chunk of message for an unknown request. This can happen when a request is rejected based on headers, and then the rejected body arrives.");
+            } else {
+                ByteBuf byteBuf = content.content();
+                if (byteBuf.capacity() > 0) {
                     ByteBuf copy = byteBuf.copy();
                     ByteBuffer byteBuffer = ByteBuffer.allocate(byteBuf.capacity());
                     copy.readBytes(byteBuffer).release();
                     byteBuffer.flip();
-					state.handler.onRequestData(state.asyncContext, byteBuffer);
-				}
-				if (msg instanceof LastHttpContent) {
-					state.handler.onRequestComplete(state.asyncContext);
-				}
-			}
-		}
-	}
+                    state.handler.onRequestData(state.asyncContext, byteBuffer);
+                }
+                if (msg instanceof LastHttpContent) {
+                    state.handler.onRequestComplete(state.asyncContext);
+                }
+            }
+        }
+    }
 
     public static void send404(AsyncContext asyncContext) {
         sendPlainText(asyncContext, "404 Not Found", 404);
@@ -113,21 +127,21 @@ class MuServerHandler extends SimpleChannelInboundHandler<Object> {
     }
 
     private void handleHttpRequestDecodeFailure(ChannelHandlerContext ctx, Throwable cause) {
-		String message = "Server error";
-		int code = 500;
-		if (cause instanceof TooLongFrameException) {
-			if (cause.getMessage().contains("header is larger")) {
-				code = 431;
-				message = "HTTP headers too large";
-			} else if (cause.getMessage().contains("line is larger")) {
-				code = 414;
-				message = "URI too long";
-			}
-		}
-		FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(code), copiedBuffer(message.getBytes(UTF_8)));
-		response.headers().set(HeaderNames.CONTENT_TYPE, ContentTypes.TEXT_PLAIN);
-		response.headers().set(HeaderNames.CONTENT_LENGTH, message.length());
-		ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-	}
+        String message = "Server error";
+        int code = 500;
+        if (cause instanceof TooLongFrameException) {
+            if (cause.getMessage().contains("header is larger")) {
+                code = 431;
+                message = "HTTP headers too large";
+            } else if (cause.getMessage().contains("line is larger")) {
+                code = 414;
+                message = "URI too long";
+            }
+        }
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(code), copiedBuffer(message.getBytes(UTF_8)));
+        response.headers().set(HeaderNames.CONTENT_TYPE, ContentTypes.TEXT_PLAIN);
+        response.headers().set(HeaderNames.CONTENT_LENGTH, message.length());
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
 
 }
