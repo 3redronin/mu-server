95a5a4f181d79895341e2f2ab0180e3c99baa85e
Daniel Flower <github@danielflower.com>
2019-03-30 18:33:22 +0800

    Made the flaky client test less verbose

--- src/test/java/io/muserver/FlakyClientTest.java
+++ src/test/java/io/muserver/FlakyClientTest.java
@@ -4,7 +4,6 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Test;
 import scaffolding.ClientUtils;
@@ -16,6 +15,8 @@ import java.util.concurrent.TimeUnit;
 
 import static io.muserver.MuServerBuilder.httpServer;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static scaffolding.ClientUtils.call;
 import static scaffolding.ClientUtils.request;
 
 public class FlakyClientTest {
@@ -23,43 +24,53 @@ public class FlakyClientTest {
     private MuServer server;
 
     @Test
-    public void canHandleStuffWithoutBreaking() throws InterruptedException {
+    public void canHandleStuffWithoutBreaking() throws InterruptedException, IOException {
         server = httpServer()
             .addHandler(Method.GET, "/full", (req, resp, pp) -> {
-                resp.write("A full length message");
+                try {
+                    resp.write("A full length message");
+                } catch (Exception ignored) {
+                }
             })
             .addHandler(Method.GET, "/chunks", (req, resp, pp) -> {
                 Thread.sleep(4);
-                resp.sendChunk("Chunk one");
-                resp.sendChunk("Chunk two");
+                try {
+                    resp.sendChunk("Chunk one");
+                    resp.sendChunk("Chunk two");
+                } catch (Exception ignored) {
+                }
             })
             .start();
 
-        OkHttpClient shortReader = ClientUtils.client.newBuilder().readTimeout(10, TimeUnit.MILLISECONDS).build();
+        OkHttpClient shortReader = ClientUtils.client.newBuilder()
+            .readTimeout(10, TimeUnit.MILLISECONDS)
+            .build();
 
         int calls = 200;
         CountDownLatch latch = new CountDownLatch(calls * 2);
         for (int i = 0; i < calls; i++) {
             Callback responseCallback = new Callback() {
-                @Override
                 public void onFailure(Call call, IOException e) {
                     latch.countDown();
-
                 }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
+                public void onResponse(Call call, Response response) {
                     latch.countDown();
                     response.close();
 
                 }
             };
-            shortReader.newCall(request().url(server.uri().resolve("/full").toString()).build()).enqueue(responseCallback);
-            shortReader.newCall(request().url(server.uri().resolve("/chunks").toString()).build()).enqueue(responseCallback);
+            shortReader.newCall(request(server.uri().resolve("/full")).build()).enqueue(responseCallback);
+            shortReader.newCall(request(server.uri().resolve("/chunks")).build()).enqueue(responseCallback);
         }
 
-        assertThat(latch.await(2, TimeUnit.MINUTES), Matchers.is(true));
+        assertThat(latch.await(2, TimeUnit.MINUTES), is(true));
 
+        try (Response resp = call(request(server.uri().resolve("/full")))) {
+            assertThat(resp.body().string(), is("A full length message"));
+        }
+        try (Response resp = call(request(server.uri().resolve("/chunks")))) {
+            assertThat(resp.body().string(), is("Chunk oneChunk two"));
+        }
     }
 
 
