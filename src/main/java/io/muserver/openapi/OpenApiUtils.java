b3322acf9098ff187bf39b0b33a4a66c2462a7da
Daniel Flower <git@danielflower.com>
2021-02-12 00:13:26 +0800

    Allow null lists and maps in open api objects

--- src/main/java/io/muserver/openapi/OpenApiUtils.java
+++ src/main/java/io/muserver/openapi/OpenApiUtils.java
@@ -7,10 +7,16 @@ class OpenApiUtils {
     private OpenApiUtils() {}
 
     static <K, V> Map<K, V> immutable(Map<K, V> map) {
+        if (map == null) {
+            return null;
+        }
         return Collections.unmodifiableMap(new HashMap<>(map));
     }
 
     static <T> List<T> immutable(List<T> list) {
+        if (list == null) {
+            return null;
+        }
         return Collections.unmodifiableList(new ArrayList<>(list));
     }
 }
