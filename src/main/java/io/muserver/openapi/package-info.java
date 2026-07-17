a06a2b08c60960b96d5ac9644a34c1021de75a8e
Daniel Flower <git@danielflower.com>
2026-07-17 21:46:45 +0800

    Complete JSpecify nullness coverage

--- src/main/java/io/muserver/openapi/package-info.java
+++ src/main/java/io/muserver/openapi/package-info.java
@@ -3,4 +3,7 @@
  * <p>This package can be used to specify overview documentation when creating REST resources using
  * {@link io.muserver.rest.RestHandlerBuilder#withOpenApiDocument(io.muserver.openapi.OpenAPIObjectBuilder)}</p>
  */
+@NullMarked
 package io.muserver.openapi;
+
+import org.jspecify.annotations.NullMarked;
